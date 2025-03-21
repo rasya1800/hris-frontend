105b7d3a52a45009e0d21e1bd5f9dee98cf2643e
rasya1800 <dicky.afriansyah98@gmail.com>
2025-03-21 16:33:42 +0700

    View

--- hris-frontend/src/app/layouts/header/header.component.ts
+++ hris-frontend/src/app/layouts/header/header.component.ts
@@ -1,10 +1,20 @@
+import { CommonModule } from '@angular/common'; // WAJIB ditambahkan!
 import { Component } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-header',
+  standalone: true, // WAJIB: Menjadikan komponen ini mandiri
+  imports: [CommonModule], // Tambahkan CommonModule di sini
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+    logoUrl = 'logo/logo.png';
+    isDropdownOpen = false;
 
+    toggleDropdown() {
+      this.isDropdownOpen = !this.isDropdownOpen;
+    }
 }
--- hris-frontend/src/app/layouts/sidebar/sidebar.component.ts
+++ hris-frontend/src/app/layouts/sidebar/sidebar.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common'; // WAJIB ditambahkan!
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true, // WAJIB: Menjadikan komponen ini mandiri
+  imports: [CommonModule], // Tambahkan CommonModule di sini
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
+  activeMenu: string | null = null;
 
+  toggleSubmenu(menu: string) {
+    this.activeMenu = this.activeMenu === menu ? null : menu;
+  }
 }
